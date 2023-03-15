ef698b23aa80b07be838f0be04d2fbdd72ebf9d5
WiktorK02 <123249470+WiktorK02@users.noreply.github.com>
2023-03-15 22:58:32 +0100

    optimalization and added int to hex function

--- oled_image.cpp
+++ oled_image.cpp
@@ -9,33 +9,44 @@
 using namespace cv;
 using namespace std;
 
+void int_to_hex(int pixwrite, int i) {
+	std::ostringstream x;
+	x << "0x" << std::hex << pixwrite;
+	std::string result = x.str();
+	
+	//if condition to seperate each pixelnumber
+	if (i == 0) {
+		cout << result;
+	}
+	else {
+		cout << ","<<result;
+	}
+}
+
 int main()
 {
 	Mat image;
-	image = imread("Resized.png", IMREAD_UNCHANGED);
+	image = imread("logo.png", IMREAD_UNCHANGED);
     	resize(image, image, Size(128, 64), INTER_LINEAR);
-	//im = imwrite("Resized.png", image);//save file 
-	int itterator = 0;
 	uint8_t* pixelPtr = (uint8_t*)image.data;
 	int cn = image.channels();
 	Scalar_<uint8_t> bgrPixel;
+
+	int itterator = 0;
+
 	for (int i = 0; i < image.rows; i++)
 	{
-		int k;
 		for (int j = 0; j < image.cols; j++)
 		{
-			bgrPixel.val[0] = pixelPtr[i * image.cols * cn + j * cn + 0]; // B
-			bgrPixel.val[1] = pixelPtr[i * image.cols * cn + j * cn + 1]; // G
-			bgrPixel.val[2] = pixelPtr[i * image.cols * cn + j * cn + 2]; // R
-			//bgrPixel.val[3] = pixelPtr[i * image.cols * cn + j * cn + 3];transparecy?
-			// do something with BGR values...
-			if(bgrPixel.val[0] == 255 && bgrPixel.val[1] == 255 && bgrPixel.val[2] == 255){
-				k = 0;
-				cout << k;
+			bgrPixel.val[3] = pixelPtr[i * image.cols * cn + j * cn + 3]; // Transparecy
+			
+			if(bgrPixel.val[3] == 0){
+				int_to_hex(0, itterator);
+				itterator++;
 			}
 			else {
-				k = 1;
-				cout << k;
+				int_to_hex(1, itterator);
+				itterator++;
 			}
 		}
 	}

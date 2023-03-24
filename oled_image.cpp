c4ec669f5fd948232606581c8fd666c3cd36283d
WiktorK02 <123249470+WiktorK02@users.noreply.github.com>
2023-03-24 14:35:22 +0100

    Update oled_image.cpp

--- oled_image.cpp
+++ oled_image.cpp
@@ -14,7 +14,8 @@
 using namespace cv;
 using namespace std;
 
-void coutbyte(string array, int itterator, int image_width, int image_height) {
+//bin to hex converter function
+void bin_to_hex(std::string array, int itterator, int image_width, int image_height) {
 
 	std::string binaryStr = array; // binary string
 	std::bitset<8> binary(binaryStr); // convert to bitset
@@ -36,25 +37,27 @@ void coutbyte(string array, int itterator, int image_width, int image_height) {
 	}
 }
 
-int main()
-{
-	Mat image;
-	image = imread("logo.png", IMREAD_UNCHANGED);
-	resize(image, image, Size(IMAGE_WIDTH, IMAGE_HEIGHT), INTER_LINEAR);
-	uint8_t* pixelPtr = (uint8_t*)image.data;
-	int cn = image.channels();
-	Scalar_<uint8_t> bgrPixel;
+//fucnction that removes every char that is after '.' in file name 
+string remove_extension(std::string& file_name) {
+	 file_name = file_name.substr(0, file_name.find("."));
+	 return file_name;
+}
 
+//function that write whole png byte array(base on tranparency)
+void png_write_array(cv::Mat &image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t> bgrPixel, int cn, std::string file_name) {
+	
 	int itterator = 1;
 	int byteitterator = 0;
 	char valueofpixel;
 	string arr;
 	
-	cout << "const unsigned char "/*name of file here*/"[] PROGMEM = {"<<endl;
+	remove_extension(file_name);
 	
+	std::cout << "const unsigned char "<<file_name<< "[] PROGMEM = {" << endl;
+
 	for (int i = 0; i < image.rows; i++)
 	{
-		
+
 		for (int j = 0; j < image.cols; j++)
 		{
 			//bgrPixel.val[0] = pixelPtr[i * image.cols * cn + j * cn + 0]; R
@@ -63,38 +66,53 @@ int main()
 			bgrPixel.val[3] = pixelPtr[i * image.cols * cn + j * cn + 3]; // Transparecy
 
 			if (bgrPixel.val[3] == 0) {
-				
+
 				valueofpixel = '1';//if transparency = 0 return 1, else return 0
 				arr.push_back(valueofpixel);//push to 8-bit array
 			}
 			else {
-				
+
 				valueofpixel = '0';
 				arr.push_back(valueofpixel);
 			}
-			
+
 			//every 8 bits print hex number
 			if (itterator % 8 == 0) {
- 				coutbyte(arr, itterator, IMAGE_WIDTH, IMAGE_HEIGHT);
+				bin_to_hex(arr, itterator, IMAGE_WIDTH, IMAGE_HEIGHT);
 				arr = ""; // for next hexdecimal number
 				byteitterator += 1;
 
 			}
 			//endl every 16 numbers
 			if (itterator % 128 == 0) {
-				
-				cout << endl;
+
+				std::cout << endl;
 			}
 			//tab every new line
 			if (itterator % 128 == 1) {
 
-				cout << "\t";
+				std::cout << "\t";
 			}
 
 			itterator++;
 		}
 	}
 	cout << "};";//close array 
+}
+
+int main()
+{
+	cv::Mat image;
+	
+	std::string file_name = "logo.png";
+	image = imread(file_name, IMREAD_UNCHANGED);
+	resize(image, image, Size(IMAGE_WIDTH, IMAGE_HEIGHT), INTER_LINEAR);
+	
+	std::uint8_t* pixelPtr = (std::uint8_t*)image.data;
+	int cn = image.channels();
+	cv::Scalar_<uint8_t> bgrPixel;
+
+	png_write_array(image, pixelPtr, bgrPixel, cn, file_name);
 
 	return 0;
 }

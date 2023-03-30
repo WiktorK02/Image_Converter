62937d39c0c5e1d8315eb9aec265cc3b06c5c24f
WiktorK02 <123249470+WiktorK02@users.noreply.github.com>
2023-03-30 19:35:19 +0200

    update jpg and png pixel write

--- oled_image.cpp
+++ oled_image.cpp
@@ -8,13 +8,12 @@
 #include <bitset>
 #include <iomanip>
 
-#define IMAGE_WIDTH 128
-#define IMAGE_HEIGHT 64
+#define IMAGE_WIDTH 512
+#define IMAGE_HEIGHT 256
 
 using namespace cv;
 using namespace std;
 
-//bin to hex converter function
 void bin_to_hex(std::string array, int itterator, int image_width, int image_height) {
 
 	std::string binaryStr = array; // binary string
@@ -37,32 +36,23 @@ void bin_to_hex(std::string array, int itterator, int image_width, int image_hei
 	}
 }
 
-//fucnction that removes every char that is after '.' in file name 
+//fucnction that removes everything that is after '.' in file name in order not to get an error
 string remove_extension(std::string& file_name) {
 	 file_name = file_name.substr(0, file_name.find("."));
 	 return file_name;
 }
-
-//function that write whole png byte array(base on tranparency)
-void png_write_array(cv::Mat &image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t> bgrPixel, int cn, std::string file_name) {
+void png_pixel_read(cv::Mat &image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t> bgrPixel, int cn, std::string file_name) {
 	
 	int itterator = 1;
 	int byteitterator = 0;
 	char valueofpixel;
 	string arr;
-	
-	remove_extension(file_name);
-	
-	std::cout << "const unsigned char "<<file_name<< "[] PROGMEM = {" << endl;
 
 	for (int i = 0; i < image.rows; i++)
 	{
 
 		for (int j = 0; j < image.cols; j++)
 		{
-			//bgrPixel.val[0] = pixelPtr[i * image.cols * cn + j * cn + 0]; R
-			//bgrPixel.val[1] = pixelPtr[i * image.cols * cn + j * cn + 1]; G
-			//bgrPixel.val[2] = pixelPtr[i * image.cols * cn + j * cn + 2]; B
 			bgrPixel.val[3] = pixelPtr[i * image.cols * cn + j * cn + 3]; // Transparecy
 
 			if (bgrPixel.val[3] == 0) {
@@ -99,12 +89,93 @@ void png_write_array(cv::Mat &image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t
 	}
 	cout << "};";//close array 
 }
+void jpg_pixel_read(cv::Mat& image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t> bgrPixel, int cn, std::string file_name) {
+
+	int itterator = 1;
+	int byteitterator = 0;
+	char valueofpixel;
+	string arr;
+
+	int min_rgb_value = 0;
+	int max_rgb_value = 0;
+
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+			int r = static_cast<int>(pixel[0]);
+			int g = static_cast<int>(pixel[1]);
+			int b = static_cast<int>(pixel[2]);
+			
+			//check if min and max brightness
+			if ((r + g + b) > max_rgb_value) {
+				max_rgb_value = (r + g + b);
+			}
+			if ((r + g + b) < min_rgb_value) {
+				min_rgb_value = (r + g + b);
+			}
+		}
+	}
+	for (int i = 0; i < image.rows; i++) {
+		for (int j = 0; j < image.cols; j++) {
+			cv::Vec3b pixel = image.at<cv::Vec3b>(i, j);
+			int r = static_cast<int>(pixel[0]);
+			int g = static_cast<int>(pixel[1]);
+			int b = static_cast<int>(pixel[2]);
+			
+			//check if avarage value is greater specific value  
+			if ((r + g + b) <= (min_rgb_value + max_rgb_value) / 2) {
+				
+				valueofpixel = '0';
+				arr.push_back(valueofpixel);//push to 8-bit array
+			}
+			else {
+
+				valueofpixel = '1';
+				arr.push_back(valueofpixel);
+			}
+
+			//every 8 bits print hex number
+			if (itterator % 8 == 0) {
+				
+				bin_to_hex(arr, itterator, IMAGE_WIDTH, IMAGE_HEIGHT);
+				arr = ""; // for next hexdecimal number
+				byteitterator += 1;
+
+			}
+			//endl every 16 numbers
+			if (itterator % 128 == 0) {
+
+				std::cout << endl;
+			}
+			//tab every new line
+			if (itterator % 128 == 1) {
+
+				std::cout << "\t";
+			}
+
+			itterator++;
+		}
+	}
+}
+void write_array(cv::Mat& image, std::uint8_t* pixelPtr, cv::Scalar_<uint8_t> bgrPixel, int cn, std::string file_name) {
+
+	remove_extension(file_name);
+	std::cout << "const unsigned char " << file_name << "[] PROGMEM = {" << endl;
+	
+	//if image is .jpg do:
+	jpg_pixel_read(image, pixelPtr, bgrPixel, cn, file_name);
+
+	//if image is .png do:
+	//png_pixel_read(image, pixelPtr, bgrPixel, cn, file_name);
+
+	cout << "};";//close array 
+}
 
 int main()
 {
 	cv::Mat image;
 	
-	std::string file_name = "logo.png";
+	std::string file_name = "img.jpg";
 	image = imread(file_name, IMREAD_UNCHANGED);
 	resize(image, image, Size(IMAGE_WIDTH, IMAGE_HEIGHT), INTER_LINEAR);
 	
@@ -112,7 +183,6 @@ int main()
 	int cn = image.channels();
 	cv::Scalar_<uint8_t> bgrPixel;
 
-	png_write_array(image, pixelPtr, bgrPixel, cn, file_name);
-
+	write_array(image, pixelPtr, bgrPixel, cn, file_name);
 	return 0;
 }
